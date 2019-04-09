ec77a2422acbfd845c1022cb0ff2f4fcad8ea00a
sarvinder <savi895@gmail.com>
2019-04-09 15:26:19 -0800

    merge button added
    add the merge button

--- webapp/application.py
+++ webapp/application.py
@@ -6,13 +6,7 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
-@app.route('/merge', methods=['GET','POST'])
-def getData():
-    if request.method == 'POST':
-       ## print('the data is : ',(request.data[0]))
-        print((request.data))
-    return 'got data'
-
-
+def mergeTheFile():
+    pass
 if __name__ == "__main__":
     app.run(debug=True)
